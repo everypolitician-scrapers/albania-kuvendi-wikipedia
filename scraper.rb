5b31ae33de47ef4abe5ba6ef92b76239e475d916
Tony Bowden <tony@tmtm.com>
2018-01-25 10:14:47 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,5 @@
 #!/bin/env ruby
-# encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'pry'
@@ -10,26 +10,26 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def scrape_term_8(url)
   noko = noko_for(url)
   noko.xpath('//table[.//th[contains(.,"Lista e Deputeteve")]]//ul//li').each do |li|
-    data = { 
-      name: li.at_xpath('.//text()').text.tidy,
+    data = {
+      name:         li.at_xpath('.//text()').text.tidy,
       wikiname__sq: li.xpath('.//a[not(@class="new")]/@title').text,
-      area: li.xpath('preceding::b').last.text.gsub('Qarku ','').tidy,
-      party: li.xpath('preceding::p').last.text.tidy,
-      term: '8',
-      source: url,
+      area:         li.xpath('preceding::b').last.text.gsub('Qarku ', '').tidy,
+      party:        li.xpath('preceding::p').last.text.tidy,
+      term:         '8',
+      source:       url,
     }
-    ScraperWiki.save_sqlite([:name, :area, :party, :term], data)
+    ScraperWiki.save_sqlite(%i[name area party term], data)
   end
 end
 
@@ -39,20 +39,18 @@ def scrape_term_7(url)
     constituency = ctable.xpath('preceding::h2/span[@class="mw-headline"]').last.text
     ctable.xpath('.//tr[td]').each do |tr|
       tds = tr.css('td')
-      data = { 
-        name: tds[1].css('a').first.text.tidy,
+      data = {
+        name:         tds[1].css('a').first.text.tidy,
         wikiname__en: tds[1].xpath('.//a[not(@class="new")]/@title').text,
-        area: constituency,
-        party: tds[2].css('a').first.text.tidy,
-        term: '7',
-        source: url,
+        area:         constituency,
+        party:        tds[2].css('a').first.text.tidy,
+        term:         '7',
+        source:       url,
       }
-      ScraperWiki.save_sqlite([:name, :area, :party, :term], data)
+      ScraperWiki.save_sqlite(%i[name area party term], data)
     end
   end
 end
 
-
 scrape_term_8('https://sq.wikipedia.org/wiki/Kuvendi_i_Shqip%C3%ABris%C3%AB')
 scrape_term_7('https://en.wikipedia.org/wiki/List_of_members_of_the_parliament_of_Albania,_2009%E2%80%932013')
-
